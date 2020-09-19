e48023cc5b36b8231cd39b8ea41a1d009c4c185a
CatL <catherinelui.2@gmail.com>
2020-09-19 14:22:10 -0600

    debugging

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bansai Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheese Burger", FALSE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Pounders", TRUE);
