fe231cd32d24cec08e525d7a0406a27903155930
CatL <catherinelui.2@gmail.com>
2020-09-20 21:40:18 -0600

    updated heroku JAWSDB connections

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "rootpw",
-    database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'rootpw',
+        database: 'burgers_db'
+    });
+};
 
 //Make connection
 connection.connect(function(err) {
